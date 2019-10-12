861ced98b5f9b303ffde1cafcceeac7c54a75154
Ashley Norton <nortonashleym@gmail.com>
2019-10-12 02:43:30 +0000

    updated the list_conferences and menu methods

--- lib/nfl_standings/cli.rb
+++ lib/nfl_standings/cli.rb
@@ -1,20 +1,36 @@
 class NflStandings::CLI 
   
   def call
-    puts "The NFL Standings as of today are:"
     list_conferences
     menu
+    goodbye
   end 
   
   def list_conferences
+    puts "The NFL Standings as of today are:"
     #Display 2 options for NFL conferences 
     puts "\n    1. American Football Conference (AFC)\n
     2. National Football Conference (NFC)"
   end 
   
   
-  def menu 
-    puts "\nClick the appropriate number to view the conference standings"
+  def menu
+    input = nil 
+    while input != "exit"
+      puts "\nClick the appropriate number to view the conference standings, type list to pick a different conference again, or type exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "AFC Standings:\n1. Houston Texans -- Wins: 1 Losses: 3 Ties: 0\n2. Miami Dolphins -- Wins: 3 Losses: 1 Ties: 0"
+      when "2"
+        puts "NFC Standings:\n1. Atlanta Falcons -- Wins: 1 Losses: 3 Ties: 0\n2. Seattle Seahawks -- Wins: 3 Losses: 1 Ties: 0"
+      when "list"
+        list_conferences
+      end
+    end 
   end 
   
+  def goodbye
+    puts "Check back tomorrow for the latest updates!"
+  end 
 end 
