198d1b0dc268d0941640bbe161040c5164a0f054
Ashley Norton <nortonmonet@gmail.com>
2019-10-13 14:22:54 -0400

    added the beginnings of print methods in cli.rb file

--- lib/nfl_standings/cli.rb
+++ lib/nfl_standings/cli.rb
@@ -1,24 +1,26 @@
-class NflStandings::CLI 
-  
+class NflStandings::CLI
+
   def call
     list_conferences
     menu
     goodbye
-  end 
-  
+  end
+
   def list_conferences
     puts "The NFL Standings as of today are:"
-    #Display 2 options for NFL conferences 
+    #Display 2 options for NFL conferences
     puts "\n    1. All NFL Team Standings\n\n    2. American Football Conference (AFC)\n
     3. National Football Conference (NFC)"
-  end 
-  
-  
+  end
+
+
   def menu
-    input = nil 
+    input = nil
     while input != "exit"
-      puts "\nClick the appropriate menu number to view the standings, type list to go back to the menu, or type exit\n\n"
+      puts ""
+      puts "Click the appropriate menu number to view the standings, type list to go back to the menu, or type exit\n\n"
       input = gets.strip.downcase
+
       case input
       when "1"
         puts "\n    1. Atlanta Falcons -- Wins: 1 Losses: 3 Ties: 0\n    2. Houston Texans -- Wins: 1 Losses: 3 Ties: 0\n    3. Miami Dolphins -- Wins: 3 Losses: 1 Ties: 0\n    4. Seattle Seahawks -- Wins: 3 Losses: 1 Ties: 0"
@@ -29,13 +31,38 @@ class NflStandings::CLI
       when "list"
         puts "\n"
         list_conferences
-      else 
+      else
         puts "\nThat's not an option. Type list to return to the menu or type exit."
       end
-    end 
-  end 
-  
+    end
+  end
+
   def goodbye
     puts "\nCheck back tomorrow for the latest updates!"
-  end 
-end 
+  end
+
+  def print_standings
+    puts
+  end
+
+  def print_all_standings
+    puts ""
+    puts "---------- All NFL Team Standings ----------"
+    puts ""
+  end
+
+  def print_afc_standings
+    puts ""
+    puts "---------- AFC Standings ----------"
+    puts ""
+  end
+
+  def print_nfc_standings
+    puts ""
+    puts "---------- NFC Standings ----------"
+    puts ""
+  end
+
+
+
+end
