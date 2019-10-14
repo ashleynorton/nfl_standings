d75f36e110f295639651e5dfd46f1e01a142f012
Ashley Norton <nortonmonet@gmail.com>
2019-10-14 14:31:07 -0400

    added scraping method to standing.rb

--- lib/nfl_standings/standing.rb
+++ lib/nfl_standings/standing.rb
@@ -3,27 +3,12 @@ class NflStandings::Standing
   attr_accessor :team_name, :wins, :losses, :ties
 
   def self.all_nfl_teams_standings
-    #I should return all nfl teams stats
+    #Scrape espn/nfl/standings and then return standings based on that data
     puts ""
     puts "---------- All NFL Team Standings ----------"
     puts ""
 
-    #puts "1. Atlanta Falcons -- Wins: 1 Losses: 3 Ties: 0"
-    #puts "2. Buffalo Bills -- Wins: 3 Losses: 1 Ties: 0"
-
-    standing_1 = self.new
-    standing_1.team_name = "Atlanta Falcons"
-    standing_1.wins = "1"
-    standing_1.losses = "5"
-    standing_1.ties = "0"
-
-    standing_2 = self.new
-    standing_2.team_name = "Atlanta Falcons"
-    standing_2.wins = "1"
-    standing_2.losses = "5"
-    standing_2.ties = "0"
-
-    [standing_1, standing_2]
+    self.scrape_all_standings
   end
 
   def self.all_afc_teams_standings
@@ -68,4 +53,31 @@ class NflStandings::Standing
     [standing_1, standing_2]
   end
 
+  def self.scrape_all_standings
+    standings = []
+
+    standings << self.scrape_espn
+
+    standings
+  end
+
+  def self.scrape_espn
+    doc = Nokogiri::HTML(open("https://www.espn.com/nfl/standings"))
+
+    standing = self.new
+    standing.team_name = doc.css(".ResponsiveTable").css(".hide-mobile").css("a")[1].text
+    standing.wins = doc.css(".Table__TD").css(".stat-cell")[0].text
+    standing.losses = doc.css(".Table__TD").css(".stat-cell")[1].text
+    standing.ties = doc.css(".Table__TD").css(".stat-cell")[2].text
+
+    standing
+  end
+
+  def self.scrape_afc_standings
+
+  end
+
+  def self.scrape_nfc_standings
+
+  end
 end
