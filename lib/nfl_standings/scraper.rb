3059d6d9812d451701cf56695fc187028910f87c
Ashley Norton <nortonmonet@gmail.com>
2020-01-24 22:26:45 -0500

    Refactored code to loop through the data properly and fixed the formatting.

--- lib/nfl_standings/scraper.rb
+++ lib/nfl_standings/scraper.rb
@@ -1,35 +1,29 @@
-require_relative "standing.rb"
-
 class NflStandings::Scraper
 
+  def self.get_all
 
+    doc = Nokogiri::HTML(open("https://www.foxsports.com/nfl/standings"))
 
+    index = 0
 
-#binding.pry
+    team_list = doc.css(".wisbb_fvStand .wisbb_fullTeam").each do |elem|
+      team = elem.css("img").attr("alt").value
 
-def self.get_all
+      result = doc.css(".wisbb_fvStand").css("td").css(".wisbb_priorityColumn").children
 
-  doc = Nokogiri::HTML(open("https://www.foxsports.com/nfl/standings"))
-  index = 0
+      if index < result.length - 3
+        win = result[index].text
+        losses = result[index + 1].text
+        ties = result[index + 2].text
+        conference = "AFC"
 
-  t = doc.css(".wisbb_fvStand .wisbb_fullTeam").each do |elem|
-  binding.pry
-
-    team = elem.css("img").attr("alt").value
-    result = doc.css(".wisbb_fvStand").css("td").css(".wisbb_priorityColumn").children
-    if index < result.length - 3
-
-      win = result[index].text
-      losses = result[index + 1].text
-      ties = result[index + 2].text
-      conference = "AFC"
-      if index >= 64
-        conference = "NFC"
+        if index >= 64
+          conference = "NFC"
+        end
+        NflStandings::Standing.new(team, win, losses, ties, conference)
+        index = index + 4
       end
-      NflStandings::Standing.new(team, win, losses, ties, conference)
-
-      index = index + 4
     end
   end
-end
+
 end
